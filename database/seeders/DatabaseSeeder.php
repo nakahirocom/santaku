0fb19794efb6fe6f30a556e503027e35c5b6be61
nakahiro <nakahirocom@gmail.com>
2024-12-02 23:09:42 +0900

    p59までlarabel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
+        $this->call([TweetSeeder::class]);
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
